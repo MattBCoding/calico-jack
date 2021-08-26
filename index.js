6de5bcd9c9ca463868202522618d34e19c171091
MattBCoding <mattbodden@googlemail.com>
2021-08-26 21:32:18 +0000

    Add index.js file content from new CI PP3 template - all code copied across

--- index.js
+++ index.js
@@ -1,84 +1,30 @@
+// ===================================================
+// Total.js start script
+// https://www.totaljs.com
+// ===================================================
 
-const PythonShell = require('python-shell')['PythonShell'];
-const static = require('node-static');
-const http = require('http');
-const fs = require('fs');
+const options = {};
 
-var static_serve = new(static.Server)('./static');
+// options.ip = '127.0.0.1';
+options.port = parseInt(process.env.PORT);
+// options.unixsocket = require('path').join(require('os').tmpdir(), 'app_name');
+// options.config = { name: 'Total.js' };
+// options.sleep = 3000;
+// options.inspector = 9229;
+// options.watch = ['private'];
+// options.livereload = 'https://yourhostname';
 
-const server = http.createServer(function (req, res) {
-    static_serve.serve(req, res);
-})
+// Enables cluster:
+// options.cluster = 'auto';
+// options.cluster_limit = 10; // max 10. threads (works only with "auto" scaling)
 
-const io = require('socket.io')(server);
-io.on('connection', (socket) => {
-    console.log("Socket Connected");
-    
-    function run_python_script() {
-        try {
-            let pyshell = new PythonShell('run.py');
+// Enables threads:
+// options.cluster = 'auto';
+// options.cluster_limit = 10; // max 10. threads (works only with "auto" scaling)
+// options.timeout = 5000;
+// options.threads = '/api/';
+// options.logs = 'isolated';
 
-            socket.on('disconnect', () =>  {
-                console.log("Socket Disconnected");
-                try {
-                    pyshell.kill();
-                } catch (e) {
-                    console.log('Cannot send any more to pyshell', e);
-                }
-            });
-
-            socket.on('command_entered', (command) =>  {
-                console.log("Socket Command: ", command);
-                try {
-                    pyshell.send(command);
-                } catch (e) {
-                    console.log('Cannot send any more to pyshell', e);
-                }
-            });
-
-
-            // sends a message to the Python script via stdin
-
-            pyshell.on('message',  (message) => {
-                // received a message sent from the Python script (a simple "print" statement)
-                console.log('process Out: ', message);
-                try {
-                    socket.emit("console_output", message);
-                } catch (e) {
-                    console.log('Cannot write to socket', e);
-                }
-            });
-
-            pyshell.on('close', () => {
-                console.log('Process ended');
-            });
-
-            pyshell.on('error', (message) => {
-                console.log('Process error:', message);
-                try {
-                    socket.emit("console_output", message.traceback.replace('\n', '\r\n'));
-                } catch (e) {
-                    console.log('Cannot write to socket', e);
-                }
-            });
-        } catch (e) {
-            console.error("Exception running", e);
-        }
-    }
-
-    if (process.env.CREDS != null) {
-        fs.writeFile('creds.json', process.env.CREDS, 'utf8', function(err) {
-            if (err) {
-                console.log('Error writing file: ', err);
-                socket.emit("console_output", "Error saving credentials: " + err);
-            } else {
-                run_python_script();
-            }
-        });
-    } else {
-        run_python_script();
-    }
-});
-
-console.log('Starting node on port', process.env.PORT);
-server.listen(process.env.PORT);
+var type = process.argv.indexOf('--release', 1) !== -1 || process.argv.indexOf('release', 1) !== -1 ? 'release' : 'debug';
+// require('total4/' + type)(options);
+require('total4').http('release', options);
